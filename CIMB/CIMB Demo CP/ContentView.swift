dc50203f8ece33916251c2444f316c78b72f51e8
Atiqah Hussein <misznurulz@gmail.com>
2024-03-26 16:33:01 +0800

    add cimb

---
+++ CIMB/CIMB Demo CP/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  CIMB Demo CP
+//
+//  Created by Atiqah on 25/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingDonation = false
+    
+    var body: some View {
+        //Top and bottom homescreen
+        ZStack {
+            Image("background3")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("homescreenTop")
+                   .resizable()
+                    
+                    .frame(width: 400.0, height: 300)
+                    .padding(.bottom, 0.0)
+                 //Spacer()
+                HStack (alignment: .center, spacing: 18) {
+                 
+                    //Donation
+                    Spacer()
+                    Button(action: {showingDonation.toggle()}) {
+                        Image("donationButton3")
+                    }
+                        
+                  
+                    .sheet(isPresented: $showingDonation) {
+                      mmobDonationView()
+                    }
+                   
+                    
+                    Button {
+                        financing()
+                    } label: {
+                        Image("financingButton2")
+                    }
+                   
+                   
+                    Button {
+                        transfer()
+                    } label: {
+                        Image("transferButton2")
+                            
+                    }
+                  
+                   
+                    /*Button {
+                        more()
+                    } label: {
+                        Image("moreButton2")
+                    }*/
+                    Spacer()
+                }
+                Spacer()
+                
+                Image("homescreenBottom")
+                    .resizable()
+                    .frame(width: 400.0, height: 340)
+                    .padding(.top, 0.0)
+                
+                
+                
+            }
+        }
+        
+        
+    }
+
+    
+    //function financing
+    func financing () {
+        print("Financing Journey")
+    }
+    
+    //function doctor
+    func transfer () {
+        print("Empty Transfer Journey")
+    }
+    
+    //function More
+    func more () {
+        print("Empty More Journey")
+    }
+    
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CIMB/CIMB Demo CP/mmobClientView.swift
@@ -0,0 +1,50 @@
+//
+//  mmobClientView.swift
+//  CIMB Demo CP
+//
+//  Created by Atiqah on 25/03/2024.
+//
+
+import Foundation
+import SwiftUI
+import MmobClient
+
+struct mmobDonationView: UIViewRepresentable {
+    
+    @AppStorage("instance_domain") var instance_domain: String = "mmob"
+    let client = MmobClient()
+    
+    func makeUIView(context: Context) -> MmobClientView  {
+        let configuration = MmobIntegration(
+          configuration: MmobIntegrationConfiguration(
+            cp_id: "cp_TMaiVSyyzBx2rZqF-PqdY",
+            integration_id: "cpd_H0MqYDq5WwVRNRS30gznC",
+            environment: "stag" //  "stag" or "prod", defaults to "prod"
+          ),
+          customer: MmobCustomerInfo(
+            email: "james.tan7@example.com",
+            title: "Mr", first_name: "James",
+            surname: "Tan",
+            building_number: "Flat 82",
+            address_1: "99 Block Square",
+            town_city: "Milton Keynes",
+            postcode: "MK1 1AU"
+        )
+          )
+        
+
+        return client.loadIntegration(mmobConfiguration: configuration, instanceDomain: InstanceDomain.MMOB)
+    }
+
+    func updateUIView(_ uiView: MmobClientView, context: Context) {
+        print("update UI")
+
+    }
+    
+}
+
+struct mmobDonationView_Previews: PreviewProvider {
+    static var previews: some View {
+        mmobDonationView()
+    }
+}
